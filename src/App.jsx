a725877ff5346ec342e61e1a9343b50457e8d427
Deepak kumar kar <94552919+Captdeepak-9153@users.noreply.github.com>
2024-03-19 17:35:20 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import "./App.css";
 
 function App() {
   //here i can use useState and all the Hooks to make changes on todo app of adding list and removing list and all other opearations
-
   return (
     <>
       <div className="container mx-auto p-6 lg:w-3/4 xl:w-1/2 bg-gradient-to-r from-yellow-300 to-orange-400 rounded-lg shadow-lg">
